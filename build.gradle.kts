0d8df3280ff375118ba0ad3c93335daf9d5b1bd4
joker <fugui@axzo.cn>
2021-02-22 15:40:05 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ repositories {
 }
 dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
+    compileOnly(files("libs/wizard-template.jar"))
 }
 
 // Configure gradle-intellij-plugin plugin.
